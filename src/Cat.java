c51c3e7a8e89877be44748ef5159802be7fe1523
Nick_S <solmanidin41@gmail.com>
2022-07-21 22:04:03 +0300

    от преподавателя

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Cat {
+public class Cat {
+
+    int weight;
+    String name;
+    String color;
+    int number;
+    static int count=0;
+
+    public Cat(){
+        count=count+1;
+    }
+
+    public void voice() {
+        System.out.println("Мяу!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static void print(String rave) {
+        System.out.println(rave);
+    }
+
+    public void setWeigh(int weight) {
+        this.weight = weight;
+
+    }
+
+    public static int getCount() {
+        return count;
+    }
 }
